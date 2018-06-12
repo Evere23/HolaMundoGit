d8207ae22844e7d9ffaa78473bf3f3735b31f3c8
Evere23 <Evere_23@hotmail.com>
2018-06-12 11:48:17 -0500

    primera version

---
+++ src/com/HolaMundoGit/main/HolaMundoGit.java
@@ -0,0 +1,11 @@
+package com.HolaMundoGit.main;
+
+public class HolaMundoGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("hola mundo");
+       
+	}
+
+}
